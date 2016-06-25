b914aa5860b95a66da0ffb45fd646a86b2abf288
Santiago <jsantiagoh@gmail.com>
2016-06-25 15:15:05 +0200

    Basic bootsrap layout done

---
+++ src/components/SideBar.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const SideBar = () => (
+      <div id="sidebar-wrapper">
+      <ul className="sidebar-nav">
+        <li className="sidebar-brand">
+          <a href="#">Horizon MCP</a>
+        </li>
+        <li>
+          <a href="#">Dashboard</a>
+        </li>
+        <li>
+          <a href="#">Shortcuts</a>
+        </li>
+        <li>
+          <a href="#">Overview</a>
+        </li>
+        <li>
+          <a href="#">Events</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Services</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </div>
+)
+
+export default SideBar
